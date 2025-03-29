786964544543bdf7f77f0cd90e9c435b94b3f07b
Gavin Garzia <ggarzia@mines.edu>
2025-03-28 23:58:58 -0600

    create account endpoint

--- db/create.sql
+++ db/create.sql
@@ -1,8 +1,8 @@
 --delete db file on schema change
 CREATE TABLE IF NOT EXISTS user (
     username TEXT PRIMARY KEY,
-    phone_number INT,
+    phone_number INT NOT NULL,
     email TEXT,
-    zipcode INT,
+    zipcode INT NOT NULL,
     bio TEXT
 );

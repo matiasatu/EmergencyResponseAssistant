abd7ba303dae8b897ca4525c8acdc3f6cae93324
Gavin Garzia <ggarzia@mines.edu>
2025-03-29 00:29:32 -0600

    get_relevant_user func

--- backend/api.py
+++ backend/api.py
@@ -1,29 +1,28 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import sqlite3
-from contextlib import contextmanager
+import json
 
 app = FastAPI()
 
 class Account(BaseModel):
     username: str
-    email: str
     phone_number: int
+    email: str
     zipcode: int
     bio: str
 
 @app.on_event("startup")
 def root():
     global con
-    con = sqlite3.connect("emergencyResponseAssistant.db", check_same_thread=False)
+    con = sqlite3.connect("db/emergencyResponseAssistant.db", check_same_thread=False)
 
     cur = con.cursor()
 
-    with open("../db/create.sql", "r") as f:
+    with open("db/create.sql", "r") as f:
         script = f.read()
         cur.executescript(script)
 
-@app.get("/text")
 def send_text(text_str: str):
     ...
 
@@ -32,3 +31,27 @@ def create_account(a: Account):
     cur = con.cursor()
     cur.execute("INSERT INTO user VALUES(?,?,?,?,?)", [a.username, a.phone_number, a.email, a.zipcode, a.bio])
     con.commit()
+
+def get_relevant_users(zipcode: int) -> list[dict]:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM user WHERE zipcode=?", [zipcode])
+
+    output = []
+    for x in cur.fetchall():
+        account = {
+            "username": x[0],
+            "phone_number": x[1],
+            "email": x[2],
+            "zipcode": x[3],
+            "bio": x[4]
+        }
+        output += [account]
+
+    return output
+
+# TESTING FUNCS ----------------------------------
+
+
+@app.get("/test_get_users")
+def get_users():
+    print(get_relevant_users(10000))

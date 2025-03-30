7679ac925f01f32ccbca203005eefc2d6f652fd9
matiasatu <matiasatu@gmail.com>
2025-03-29 18:45:30 -0600

    made homepage text larger

--- frontend-ERA/src/routes/+page.svelte
+++ frontend-ERA/src/routes/+page.svelte
@@ -27,21 +27,21 @@
 
   <div class="relative flex flex-col justify-center items-center text-white text-center px-4 z-10">
     <div class="flex items-center justify-center">
-      <h1 class="pb-1 text-6xl font-bold">guardian</h1>
-      <h1 class="pb-1 text-6xl font-bold brand-text">.ai</h1>
+      <h1 class="pb-1 text-7xl font-bold">guardian</h1>
+      <h1 class="pb-1 text-7xl font-bold brand-text">.ai</h1>
     </div>
 
-    <h2 class="pb-10 text-2xl">an emergency response assistant</h2>
+    <h2 class="pb-10 text-3xl">an emergency response assistant</h2>
 
     <div class="w-full md:w-3/4 space-y-4">
-      <p class="text-lg">
+      <p class="text-xl">
         Sign up to receive instant alerts about emergencies in your area— 
-        <span class="text-xl font-bold brand-text">tailored specifically to you.</span>
+        <span class="text-2xl font-bold brand-text">tailored specifically to you.</span>
       </p>
 
-      <p class="text-lg">
+      <p class="text-xl">
         guardian.ai harnesses the speed of modern AI to deliver personalized disaster guidance and help 
-        <span class="text-xl font-bold brand-text">keep you safe</span> when it matters most.
+        <span class="text-2xl font-bold brand-text">keep you safe</span> when it matters most.
       </p>
     </div>
   </div>

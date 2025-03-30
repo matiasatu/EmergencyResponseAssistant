dd792785c099925a94b59eb23ae5f0c574ed1695
matiasatu <matiasatu@gmail.com>
2025-03-30 03:27:53 -0600

    formatting for buifeedback

--- frontend-ERA/src/routes/create-account/+page.svelte
+++ frontend-ERA/src/routes/create-account/+page.svelte
@@ -96,7 +96,7 @@
       data = JSON.parse(data)
       console.log("called groq")
       if(data.new_response){
-        bioFeedback = data.response.replace(/-/g, '\n-');
+        bioFeedback = data.response.replace('-', '\n-');
       }
       return;
 

c11ba2f144748e319f48174f9724be42ac029331
bancell04 <bancell@mines.edu>
2025-03-30 03:07:48 -0600

    add onMount to login/username

--- frontend-ERA/src/routes/login/[username]/+page.svelte
+++ frontend-ERA/src/routes/login/[username]/+page.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
-    import * as login from "../../../lib/login"
+    import * as login from "../../../lib/login";
     import { goto } from "$app/navigation";
-
+    import { onMount } from "svelte";
+  
     export let data: {
       username: string;
     };
-
-    login.change_user(data.username)
-
-    goto("/current-disaster")
-
+  
+    onMount(() => {
+      login.change_user(data.username);
+      goto("/current-disaster");
+    });
+    
 </script>
   
 <h1>Welcome, {data.username}!</h1>
